7af0845378846e01d80242f59f22f76e66df5587
Your Name <mdhmaktaruzzaman9101@gmail.com>
2023-09-23 11:12:31 +0600

    typescript

---
+++ Object/ob.js
@@ -0,0 +1,13 @@
+var user1 = {
+    name: "Himu",
+    age: 25
+};
+console.log(user1);
+// ==================================== 1
+var user2;
+user2 = {
+    name: "Hello",
+    age: 22
+};
+console.log(user2);
+// ==================================== 2
---
+++ Object/ob.ts
@@ -0,0 +1,14 @@
+let user1 = {
+    name: "Himu",
+    age: 25
+}
+console.log(user1)
+
+// ==================================== 1
+let user2:{name: string, age: number}
+user2 = {
+    name: "Hello",
+    age: 22
+}
+console.log(user2)
+// ==================================== 2
